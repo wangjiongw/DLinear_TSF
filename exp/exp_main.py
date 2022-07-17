b440308fbacdcd94d45690bb0a05b70aa9282d53
WANG Jiong <wangjiong@link.cuhk.edu.hk>
2022-07-17 21:13:03 +0800

    print mape

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -286,10 +286,10 @@ class Exp_Main(Exp_Basic):
             os.makedirs(folder_path)
 
         mae, mse, rmse, mape, mspe, rse, corr = metric(preds, trues)
-        print('mse:{}, mae:{}, rse:{}, corr:{}'.format(mse, mae, rse, corr))
+        print('mse:{}, mae:{}, mape: {}, rse:{}, corr:{}'.format(mse, mae, mape, rse, corr))
         f = open("result.txt", 'a')
         f.write(setting + "  \n")
-        f.write('mse:{}, mae:{}, rse:{}, corr:{}'.format(mse, mae, rse, corr))
+        f.write('mse:{}, mae:{}, mape: {}, rse:{}, corr:{}'.format(mse, mae, mape, rse, corr))
         f.write('\n')
         f.write('\n')
         f.close()
